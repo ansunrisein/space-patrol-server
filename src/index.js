988bdb6306e0761bc85cdbbd55c23e106e8f00cd
heiNina <nina.shabrova2001@gmail.com>
2020-04-13 01:48:08 +0300

    Image router injected

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Mongoose = require('mongoose');
 
 const articleRouter = require('./routers/article');
+const imageRouter = require('./routers/image');
 
 Mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
@@ -12,8 +13,10 @@ Mongoose.connect(process.env.DB_URL, {
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
+app.use(express.static('public'));
 
 app.use('/api', articleRouter);
+app.use('/api', imageRouter);
 
 app.listen(3001, () => console.log('Started'));
